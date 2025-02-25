0fd63459e5a68292d26e3a02e9b3573be191e231
Sanant Tudu <Sanat.Tudu@winjittechnologiespvtltd.local>
2025-02-25 12:04:26 +0530

    fixed nav and scoll animation

--- src/pages/Navigation/index.tsx
+++ src/pages/Navigation/index.tsx
@@ -1,28 +1,19 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import { AiOutlineMenu, AiFillHome } from "react-icons/ai";
-import { FaProjectDiagram } from "react-icons/fa";
-import { BiSolidContact } from "react-icons/bi";
-import { BsFillInfoCircleFill } from "react-icons/bs";
-import { GiSkills } from "react-icons/gi";
-import { IoSchoolSharp } from "react-icons/io5";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
+import { motion, useScroll, useSpring } from "framer-motion";
+import Sidebar from "./sidebar";
 
 const NavBar = () => {
-  const [nav, setNav] = useState(false);
-  const [shadow, setShadow] = useState(false);
+  const [shadow, setShadow] = useState(true);
   const router = useRouter();
-  const handleNav = () => {
-    setNav((prev) => !prev);
-  };
 
   useEffect(() => {
     const handleShadow = () => {
-      if (window.scrollY >= 90) {
-        setShadow(true);
-      } else {
+      if (window.scrollY >= 500) {
         setShadow(false);
+      } else {
+        setShadow(true);
       }
     };
     window.addEventListener("scroll", handleShadow);
@@ -37,79 +28,54 @@ const NavBar = () => {
     { title: "Contact", link: "#contact" },
   ];
 
-  const linksIcons = [
-    { title: "Home", link: "/", icon: <AiFillHome /> },
-    { title: "About", link: "#about", icon: <BsFillInfoCircleFill /> },
-    { title: "Skills", link: "#skills", icon: <GiSkills /> },
-    { title: "Background", link: "#background", icon: <IoSchoolSharp /> },
-    { title: "Projects", link: "#projects", icon: <FaProjectDiagram /> },
-    { title: "Contact", link: "#contact", icon: <BiSolidContact /> },
-  ];
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
 
   return (
     <div
       className={
         shadow
-          ? " bg-[rgba(37,61,25,0.8)] fixed w-full h-20 l z-[100] ease-in-out duration-700"
+          ? " bg-green-950 fixed w-full h-20 l z-[100] ease-in-out duration-700 hidden md:flex"
           : "fixed w-full h-20 z-[100] "
       }
     >
-      <div className="flex justify-end md:justify-center items-center w-full h-full px-2">
-        <div className="text-green-300">
-          <ul className=" hidden md:flex ">
-            {links.map((link) => {
-              return (
-                <div
-                  key={link.title}
-                  onClick={() => {
-                    router.push(link.link);
-                  }}
-                  className="cursor-pointer"
-                >
-                  <li className="ml-10 text-sm uppercase font-bold hover:text-white">
-                    {link.title}
-                  </li>
-                </div>
-              );
-            })}
-          </ul>
-          <div onClick={handleNav} className="md:hidden hover:text-white">
-            <AiOutlineMenu></AiOutlineMenu>
-          </div>
-        </div>
-      </div>
-      <div
-        className={
-          nav ? " md:hidden fixed left-0 top-0 w-full h-screen bg-black/90" : ""
-        }
-      >
-        <div
-          className={
-            nav
-              ? "md:hidden fixed left-0 top-[25%] w-[5%] sm:w-[5%] md:w-[5%] max-h-max-content rounded-xl bg-[rgb(11,11,11)] p-6 ease-in duration-600"
-              : "fixed left-[-100%] top-0 p-10 ease-in duration-600"
-          }
-        >
-          <div className="py-2">
-            <ul className="uppercase flex flex-col  items-center">
-              {linksIcons.map((link) => {
+      <motion.div style={{ scaleX }} className="h-2 bg-white" />
+      {shadow ? (
+        <div className="flex justify-end md:justify-center items-center w-full h-full px-2">
+          <div className="text-green-300">
+            <ul className=" hidden md:flex ">
+              {links.map((link) => {
                 return (
-                  <Link key={link.title} href={link.link}>
-                    <li
-                      onClick={() => {
-                        setNav(false);
-                      }}
-                      className="py-3 text-2xl hover:text-purple-700 text-sky-700 "
-                    >
-                      {link.icon}
+                  <div
+                    key={link.title}
+                    onClick={() => {
+                      router.push(link.link);
+                    }}
+                    className="cursor-pointer"
+                  >
+                    <li className="ml-10 text-sm uppercase font-bold hover:text-white">
+                      {link.title}
                     </li>
-                  </Link>
+                  </div>
                 );
               })}
             </ul>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="text-white">
+          <Sidebar />
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/pages/Navigation/links.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+
+interface LinksProps {
+  handleToggle: () => void;
+}
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const linksList = [
+  {
+    label: "Home",
+    value: "home",
+  },
+  {
+    label: "About",
+    value: "about",
+  },
+  {
+    label: "Skills",
+    value: "skills",
+  },
+
+  {
+    label: "Background",
+    value: "background",
+  },
+  {
+    label: "Projects",
+    value: "projects",
+  },
+  {
+    label: "Contact",
+    value: "contact",
+  },
+];
+
+const linkVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
+const Links = ({ handleToggle }: LinksProps) => {
+  const router = useRouter();
+
+  return (
+    <motion.div
+      variants={variants}
+      className={
+        "absolute w-full h-full flex flex-col items-center justify-center gap-5"
+      }
+    >
+      {linksList.map((item) => (
+        <>
+          <div
+            key={item.value}
+            className="text-xl py-4 md:text-4xl hover:scale-110 hover:transition-transform w-full flex justify-center cursor-pointer bg-green-400"
+            onClick={() => {
+              router.push(`#${item.value}`);
+              handleToggle();
+            }}
+          >
+            <motion.span
+              variants={linkVariants}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{
+                scale: 0.95,
+              }}
+            >
+              {item.label}
+            </motion.span>
+          </div>
+        </>
+      ))}
+    </motion.div>
+  );
+};
+
+export default Links;
